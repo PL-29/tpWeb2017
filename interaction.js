2d26cd04eef16b4d7434371dbf46a101c40e784d
16005310 <16005310@e010m05.istic.univ-rennes1.fr>
2017-01-31 09:51:33 +0100

    Ajout des fonctions presser, deplacer, relacher

--- interaction.js
+++ interaction.js
@@ -3,9 +3,43 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
+	var x0 = 0;
+	var x1 = 0;
+	var y0 = 0;
+	var y1 = 0;
+	var bool = false;
+	
+	canvas.addEventListener("mousedown", presser);
+	canvas.addEventListener("mousemove", deplacer);
+	canvas.addEventListener("mouseup", relacher); 
 
 	// Developper les 3 fonctions gérant les événements
-
+	function presser(evt){
+		console.log("presser");
+		bool = true;
+		var coordonnees = getMousePosition(canvas, evt);
+		this.x0 = coordonnees.x;
+		this.y0 = coordonnees.y;
+	}
+	
+	function deplacer(evt){
+		if(bool){
+			console.log("deplacer");
+			var coordonnees = getMousePosition(canvas, evt);
+			this.x1 = coordonnees.x;
+			this.y1 = coordonnees.y;	
+		}
+	}
+	
+	function relacher(evt){
+		if(bool){
+			console.log("relacher");
+			var coordonnees = getMousePosition(canvas, evt);
+			this.x1 = coordonnees.x;
+			this.y1 = coordonnees.y;
+			bool = false;
+		}
+	}
 	// Associer les fonctions précédentes aux évènements du canvas.
 };
 
@@ -18,6 +52,9 @@ function getMousePosition(canvas, evt) {
     y: evt.clientY - rect.top
   };
 };
+
+
+
 
 
 
